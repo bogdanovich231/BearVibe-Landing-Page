352697146c425fe2a2a7b730e2591112706d1a2a
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2024-01-28 23:33:39 +0100

    refactor: deploy

--- backend/config/database.js
+++ backend/config/database.js
@@ -50,11 +50,11 @@ module.exports = ({ env }) => {
     postgres: {
       connection: {
         connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'localhost'),
+        host: env('DATABASE_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
         port: env.int('DATABASE_PORT', 5432),
         database: env('DATABASE_NAME', 'strapi'),
         user: env('DATABASE_USERNAME', 'strapi'),
-        password: env('DATABASE_PASSWORD', 'strapi'),
+        password: env('DATABASE_PASSWORD', '4BP8D.sxvx!Z.a+'),
         ssl: env.bool('DATABASE_SSL', false) && {
           key: env('DATABASE_SSL_KEY', undefined),
           cert: env('DATABASE_SSL_CERT', undefined),
