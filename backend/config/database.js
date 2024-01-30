402bd295ecff87aa613369f28b07fb9f4b3087a1
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2024-01-30 15:18:17 +0100

    fix: config

--- backend/config/database.js
+++ backend/config/database.js
@@ -3,17 +3,13 @@ const path = require('path');
 module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
-    postgres: {
-      connection: {
-        connectionString: env('DATABASE_URL'),
-        host: env('DATABASE_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
-        port: env.int('DATABASE_PORT', 6543),
-        database: env('DATABASE_NAME', 'postgres'),
-        user: env('DATABASE_USERNAME', 'postgres.uheglbvolqwnsofvstyn'),
-        password: env('DATABASE_PASSWORD', '4BP8D.sxvx!Z.a+'),
-        ssl: env.bool('DATABASE_SSL', false)
-      },
-      useNullAsDefault: true
+    connection: {
+      host: env('DATABASE_HOST', 'aws-0-eu-central-1.pooler.supabase.com'),
+      port: env.int('DATABASE_PORT', 6543),
+      database: env('DATABASE_NAME', 'postgres'),
+      user: env('DATABASE_USERNAME', 'postgres.uheglbvolqwnsofvstyn'),
+      password: env('DATABASE_PASSWORD', '4BP8D.sxvx!Z.a+'),
+      ssl: env.bool('DATABASE_SSL', false)
     },
   }
 })
