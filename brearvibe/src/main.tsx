90e37e7c6c7e2b7f6afe864f1b66679e4021ceb0
Tatsiana <96041733+bogdanovich231@users.noreply.github.com>
2024-01-31 18:58:13 +0100

    feat: implement error boundary

--- brearvibe/src/main.tsx
+++ brearvibe/src/main.tsx
@@ -4,11 +4,15 @@ import App from './App.tsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import { store } from './store/store.ts';
+import ErrorMessage from './Helper/ErrorBoundary/ErrorMessage.tsx';
+import ErrorBoundary from './Helper/ErrorBoundary/ErrorBoundary.tsx';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ErrorBoundary fallback={<ErrorMessage />}>
+        <App />
+      </ErrorBoundary>
     </Provider>
   </React.StrictMode>
 );
